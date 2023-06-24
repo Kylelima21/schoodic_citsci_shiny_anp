e14aa4c440a58f9ff87fd816c52bde26d30270ca
Kyle Lima <klima@schoodicinstitute.org>
2023-06-24 11:23:40 -0400

    first attempt of setAccountInfo

--- get_app_data.R
+++ get_app_data.R
@@ -31,6 +31,13 @@ download_photos(final_data, "www/img/obs")
 
 
 ### Deploy updates to shiny app
+## Set up account info:
+setAccountInfo(name = "si_citizen_science_display", 
+               token = "57B287A6E91E86DCCA29B542392AB8E2", 
+               secret = "6NZ1FD19vvDi6pKIQ+iMXgxiYyTsgOc2yEzTl4bC",
+               server = "shinyapps.io")
+
+## Deploy
 deployApp(launch.browser = F, forceUpdate = T)
 
 
